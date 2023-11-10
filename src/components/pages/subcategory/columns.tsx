07f1922eb3c32f55f52d0e713772c3e7e2b98480
dhruvil-codes <22ecdhr063@ldce.ac.in>
2023-11-10 09:11:39 +0530

    feat: subcategory delete

--- src/components/pages/subcategory/columns.tsx
+++ src/components/pages/subcategory/columns.tsx
@@ -1,10 +1,26 @@
-import { Subcategory } from "@/db/dexie";
+import { Subcategory, db } from "@/db/dexie";
 import { ColumnDef, Row } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "../shared/Table/DataTableColumnHeader";
 import { rankItem } from "@tanstack/match-sorter-utils";
 import { Badge } from "@/components/ui/badge";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
+import { useState } from "react";
+import { useAccount } from "../shared/AccountContext";
+import { useLiveQuery } from "dexie-react-hooks";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Trash2Icon } from "lucide-react";
 
 export const columns: ColumnDef<Subcategory>[] = [
   {
@@ -65,6 +81,10 @@ export const columns: ColumnDef<Subcategory>[] = [
     sortingFn: "datetime",
     sortDescFirst: true,
   },
+  {
+    id: "actions",
+    cell: ActionCell,
+  },
 ];
 
 function TypeCell({ row }: { row: Row<Subcategory> }) {
@@ -83,5 +103,71 @@ function TypeCell({ row }: { row: Row<Subcategory> }) {
     >
       {transType.charAt(0).toUpperCase() + transType.slice(1)}
     </Badge>
+  );
+}
+
+function ActionCell({ row }: { row: Row<Subcategory> }) {
+  const [deletable, setDeletable] = useState(false);
+  const { account } = useAccount();
+  const transactions = useLiveQuery(() => {
+    return db.transactions.where("account").equals(account.name).toArray();
+  });
+  const selectedSubCategory = row.original;
+
+  if (!transactions) {
+    return "Loading";
+  }
+
+  const handleClick = () => {
+    const hasTransactions =
+      transactions.findIndex(
+        (selectedTxn) => selectedTxn.subcategory === selectedSubCategory.name
+      ) !== -1;
+    setDeletable(!hasTransactions);
+  };
+
+  const deleteCategory = async () => {
+    if (!selectedSubCategory.id) {
+      return console.error(
+        "Delete Error: No Id found for category: ",
+        selectedSubCategory
+      );
+    }
+    await db.subcategories.delete(selectedSubCategory.id);
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="ghost" onClick={handleClick} size="icon">
+          <Trash2Icon className="h-4 w-4 text-red-600" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {deletable
+              ? "Do you want to delete this subcategory ? "
+              : "You can not delete this subcategory"}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            {deletable
+              ? "This action cannot be undone. This will permanently delete this subcategory."
+              : "beacause this category has transaction under it."}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>{deletable ? "Cancel" : "Ok"}</AlertDialogCancel>
+          {deletable && (
+            <AlertDialogAction
+              className={buttonVariants({ variant: "destructive" })}
+              onClick={deleteCategory}
+            >
+              Delete
+            </AlertDialogAction>
+          )}
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
