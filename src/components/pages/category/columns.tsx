0dc7cd3327e8b449ce3560f0a9ba0607fad55382
dhruvil-codes <22ecdhr063@ldce.ac.in>
2023-11-10 08:43:52 +0530

    fix: delete category fixed

--- src/components/pages/category/columns.tsx
+++ src/components/pages/category/columns.tsx
@@ -11,22 +11,16 @@ import {
 } from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button, buttonVariants } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Category, db } from "@/db/dexie";
 import { cn } from "@/lib/utils";
 import { rankItem } from "@tanstack/match-sorter-utils";
 import { ColumnDef, Row } from "@tanstack/react-table";
-import { MoreHorizontal, Trash2Icon } from "lucide-react";
-import { DataTableColumnHeader } from "../shared/Table/DataTableColumnHeader";
 import { format } from "date-fns";
-import { useAccount } from "../shared/AccountContext";
 import { useLiveQuery } from "dexie-react-hooks";
+import { Trash2Icon } from "lucide-react";
+import { useState } from "react";
+import { useAccount } from "../shared/AccountContext";
+import { DataTableColumnHeader } from "../shared/Table/DataTableColumnHeader";
 
 export const columns: ColumnDef<Category>[] = [
   {
@@ -96,11 +90,13 @@ function TypeCell({ row }: { row: Row<Category> }) {
 }
 
 function ActionCell({ row }: { row: Row<Category> }) {
+  const [deletable, setDeletable] = useState(false);
   const { account } = useAccount();
   const transactions = useLiveQuery(() => {
     return db.transactions.where("account").equals(account.name).toArray();
   });
   const selectedCategory = row.original;
+
   if (!transactions) {
     return "Loading";
   }
@@ -110,12 +106,51 @@ function ActionCell({ row }: { row: Row<Category> }) {
       transactions.findIndex(
         (selectedTxn) => selectedTxn.category === selectedCategory.name
       ) !== -1;
-    alert(hasTransactions);
+    setDeletable(!hasTransactions);
+  };
+
+  const deleteCategory = async () => {
+    if (!selectedCategory.id) {
+      return console.error(
+        "Delete Error: No Id found for category: ",
+        selectedCategory
+      );
+    }
+    await db.categories.delete(selectedCategory.id);
   };
 
   return (
-    <Button onClick={handleClick}>
-      <Trash2Icon className="h-4 w-4" />
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="ghost" onClick={handleClick} size="icon">
+          <Trash2Icon className="h-4 w-4 text-red-600" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {deletable
+              ? "Do you want to delete this category ? "
+              : "You can not delete this category"}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            {deletable
+              ? "This action cannot be undone. This will permanently delete this category."
+              : "beacause this category has transaction under it."}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>{deletable ? "Cancel" : "Ok"}</AlertDialogCancel>
+          {deletable && (
+            <AlertDialogAction
+              className={buttonVariants({ variant: "destructive" })}
+              onClick={deleteCategory}
+            >
+              Delete
+            </AlertDialogAction>
+          )}
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
